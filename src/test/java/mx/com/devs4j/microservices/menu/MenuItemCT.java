b3afdbbe431a28b974122552342fc2d1094c442a
Francisco Navarrete <francisco.navarrete@walmart.com>
2019-08-10 11:12:05 -0500

    MockServer

--- src/test/java/mx/com/devs4j/microservices/menu/MenuItemCT.java
+++ src/test/java/mx/com/devs4j/microservices/menu/MenuItemCT.java
@@ -1,6 +1,7 @@
 package mx.com.devs4j.microservices.menu;
 
 import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +14,13 @@ import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.test.web.client.match.MockRestRequestMatchers;
+import org.springframework.test.web.client.response.MockRestResponseCreators;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK)
@@ -23,27 +28,38 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 @DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
 public class MenuItemCT {
-	
+
 	@Autowired
 	private MockMvc mockMvc;
-	
+
+	private MockRestServiceServer mockServer;
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Before
+	public void setUp() {
+		mockServer = MockRestServiceServer.createServer(restTemplate);
+	}
+
 	@Test
 	public void shouldGetAllItems() throws Exception {
+		mockServer.expect(MockRestRequestMatchers.requestTo("http://mymockapi/latest?base=MXN&symbols=USD"))
+				.andRespond(MockRestResponseCreators.withSuccess(
+						TestUtils.readFileAsString("exchangeRate.json"), MediaType.APPLICATION_JSON));
+
 		mockMvc.perform(MockMvcRequestBuilders.get("/items"))
 			.andExpect(MockMvcResultMatchers.status().isOk());
 	}
-	
+
 	@Test
 	public void shouldCreateItem() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.post("/items")
-				.contentType(MediaType.APPLICATION_JSON)
+		mockMvc.perform(MockMvcRequestBuilders.post("/items").contentType(MediaType.APPLICATION_JSON)
 				.content(TestUtils.readFileAsString("items_post.json")));
-		
-		mockMvc.perform(MockMvcRequestBuilders.get("/items"))
-			.andExpect(MockMvcResultMatchers.status().isOk())
-			.andExpect(MockMvcResultMatchers.content()
-					.contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-			.andExpect(MockMvcResultMatchers.jsonPath("$.length()", Matchers.is(4)));
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/items")).andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.length()", Matchers.is(4)));
 	}
 
 }
