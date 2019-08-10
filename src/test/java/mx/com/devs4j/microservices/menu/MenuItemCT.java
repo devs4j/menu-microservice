530404abe032beadc2d05a935bb1a5796761d7e4
Francisco Navarrete <francisco.navarrete@walmart.com>
2019-08-10 11:03:12 -0500

    CT for item creation

--- src/test/java/mx/com/devs4j/microservices/menu/MenuItemCT.java
+++ src/test/java/mx/com/devs4j/microservices/menu/MenuItemCT.java
@@ -1,5 +1,6 @@
 package mx.com.devs4j.microservices.menu;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -29,6 +31,19 @@ public class MenuItemCT {
 	public void shouldGetAllItems() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/items"))
 			.andExpect(MockMvcResultMatchers.status().isOk());
+	}
+	
+	@Test
+	public void shouldCreateItem() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.post("/items")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(TestUtils.readFileAsString("items_post.json")));
+		
+		mockMvc.perform(MockMvcRequestBuilders.get("/items"))
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.content()
+					.contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.length()", Matchers.is(4)));
 	}
 
 }
