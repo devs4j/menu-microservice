dbfe08bada540fe00b643e82ac24b25f39331480
Francisco Navarrete <francisco.navarrete@walmart.com>
2019-08-24 02:29:36 -0500

    Added security

--- src/main/java/mx/com/devs4j/microservices/menu/MenuItemApplication.java
+++ src/main/java/mx/com/devs4j/microservices/menu/MenuItemApplication.java
@@ -3,7 +3,9 @@ package mx.com.devs4j.microservices.menu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
+@EnableResourceServer 
 @SpringBootApplication
 public class MenuItemApplication {
 
